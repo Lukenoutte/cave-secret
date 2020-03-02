09cc58c072726aac4f909d89b1f72eafe480566b
Lucas Lima <lucas.alima@souunit.com.br>
2020-03-02 18:25:17 -0300

    particulas

--- stoneSecret/Assets/Controllers/UIController.cs
+++ stoneSecret/Assets/Controllers/UIController.cs
@@ -1,18 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIController : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    public void easyButton()
     {
-        
+        SceneManager.LoadScene("GameEasy");
     }
-
-    // Update is called once per frame
-    void Update()
+    public void mediumButton()
     {
-        
+        SceneManager.LoadScene("GameMedium");
+    }
+    public void hardButton()
+    {
+        SceneManager.LoadScene("GameHard");
     }
 }
